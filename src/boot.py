7e98d1bc64967786b61d96be9c23b1537ee4c464
Visual Gadget <dm2dm@ya.ru>
2025-01-26 19:41:01 +0500

    Add WiFi status led

--- src/boot.py
+++ src/boot.py
@@ -12,21 +12,34 @@ RCS = ('PWRON_RESET', 'WDT_RESET', 'UNK2', 'UNK3', 'SOFT_RESET', 'DEEPSLEEP_RESE
 rc = machine.reset_cause()
 print('\nReset cause:', RCS[rc])
 
-# break potential boot loop by giving time to connect and fix
-import utime
-for n in range(2, -1, -1):
-	print(f'start in {n}s')
-	utime.sleep(1)
-
 if config.WIFI_AT_BOOT or config.WEB_REPL_AT_BOOT:
     import wifi
-    wf = wifi.WiFi()
-    wf.connect()
-    wf.wait_for_connection()
+    led_pin = getattr(config, 'PIN_WIFI_LED', None)
+    if led_pin is not None:
+        from machine import Pin, Signal
+        status_led = Signal(led_pin, Pin.OUT, invert=True)
+        status_led.off()
+    else:
+        status_led = None
 
-if config.WEB_REPL_AT_BOOT:
-    import webrepl
-    webrepl.start(password=config.WEB_REPL_PASSWORD)
+    wf = wifi.WiFi(status_led=status_led)
+    if not wf.connected:
+        wf.connect()
+        wf.wait_for_connection()
+
+    if config.WEB_REPL_AT_BOOT:
+        import webrepl
+        webrepl.start(password=config.WEB_REPL_PASSWORD)
+
+# break potential boot loop by giving time to connect and fix
+import utime
+try:
+    for n in range(5, -1, -1):
+        print(f'start in {n}s')
+        utime.sleep(1)
+except OSError:
+    from machine import soft_reset
+    soft_reset()
 
 import gc
 gc.collect()
