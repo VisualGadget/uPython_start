68bcf2ea82757681b23d1e19cee3f35f004fddac
Visual Gadget <dm2dm@ya.ru>
2025-02-02 18:02:57 +0500

    Add file logger

--- src/boot.py
+++ src/boot.py
@@ -1,17 +1,46 @@
 # This file is executed on every boot (including wake-boot from deep sleep)
+
 import config
 
+# Logging
 import esp
 esp.osdebug(None)
 
-# import uos
-# uos.dupterm(None, 1)  # disable REPL on UART(0)
+import os
+# os.dupterm(None, 1)  # disable REPL on UART(0)
+
+if getattr(config, 'FILE_LOGGER', False):
+    from io import IOBase
+
+    class FileLogger(IOBase):
+
+        NEW_LINE_CODE = ord('\n')
+
+        def __init__(self):
+            super().__init__()
+
+            self._f = open('device.log', 'a')
+
+        def write(self, data):
+            nb = self._f.write(data)
+            if nb and data[-1] == self.NEW_LINE_CODE:
+                self._f.flush()
+
+            return nb
+
+        def readinto(self, b: bytearray):
+            return None
+
+    os.dupterm(FileLogger(), 1)  # replace REPL on UART(0)
+
 
 import machine
 RCS = ('PWRON_RESET', 'WDT_RESET', 'UNK2', 'UNK3', 'SOFT_RESET', 'DEEPSLEEP_RESET', 'HARD_RESET')
 rc = machine.reset_cause()
 print('\nReset cause:', RCS[rc])
 
+
+# Wi-Fi
 if config.WIFI_AT_BOOT or config.WEB_REPL_AT_BOOT:
     import wifi
     led_pin = getattr(config, 'PIN_WIFI_LED', None)
@@ -31,15 +60,17 @@ if config.WIFI_AT_BOOT or config.WEB_REPL_AT_BOOT:
         import webrepl
         webrepl.start(password=config.WEB_REPL_PASSWORD)
 
+
 # break potential boot loop by giving time to connect and fix
 import utime
 try:
-    for n in range(5, -1, -1):
+    for n in range(10, -1, -1):
         print(f'start in {n}s')
         utime.sleep(1)
 except OSError:
     from machine import soft_reset
     soft_reset()
+
 
 import gc
 gc.collect()
